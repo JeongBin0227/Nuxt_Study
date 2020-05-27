6966e064e2924b3ffc77a462ad4534fdc1c6abda
JeongBin <kjh3095000@gmail.com>
2020-05-28 00:55:39 +0900

    [ADD] 서버 사이드 렌더링 준비를 위한 기본 라우터 구현

---
+++ ssr-recipe/src/components/Blue.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Blue.css';
+
+const Blue = () => {
+  return <Blue className='Blue'>Blue</Blue>;
+};
+
+export default Blue;
---
+++ ssr-recipe/src/components/Red.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Red.css';
+
+const Red = () => {
+  return <Red className='Red'>Red</Red>;
+};
+
+export default Red;
